fb6892a6c5a7565f17e92af64e6b2b06f9fd2d81
lesnikovskiy <lesnikovski@gmail.com>
2016-06-07 23:51:16 +0300

    implement post

--- CommunityApp/server.js
+++ CommunityApp/server.js
@@ -5,12 +5,40 @@ const router = express.Router();
 const fs = require("fs");
 
 app.use(express.static("./"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api", router);
 
-router.get("/eventsData", function(req, res) {
+var jobsData = [];
+fs.readFile("./sampleData/jobsData.json", "utf8", function (err, content) {
+    if (err)
+        return res.send(500, err);
+
+    jobsData = JSON.parse(content);
+});
+
+router.get("/jobsData", function (req, res) {
+    if (!jobsData.length) {
+        fs.readFile("./sampleData/jobsData.json", "utf8", function (err, content) {
+            if (err)
+                return res.send(500, err);
+            
+            jobsData = JSON.parse(content);
+
+            return res.json(jobsData);
+        });
+    } else {
+        res.json(jobsData);
+    }
+});
+
+router.route("/jobsData").post(function(req, res) {
+    console.log(req.body);
+    jobsData.push(req.body);
+});
+
+router.get("/events", function (req, res) {
     fs.readFile("./sampleData/eventsData.json", "utf8", function (err, content) {
         if (err)
             return res.send(500, err);
@@ -19,16 +47,7 @@ router.get("/eventsData", function(req, res) {
     });
 });
 
-router.get("/jobsData", function(req, res) {
-    fs.readFile("./sampleData/jobsData.json", "utf8", function (err, content) {
-        if (err)
-            return res.send(500, err);
-
-        return res.json(eval(content));
-    });
-});
-
-router.get("/jobSkills", function(req, res) {
+router.get("/jobSkills", function (req, res) {
     fs.readFile("./sampleData/jobSkills.json", "utf8", function (err, content) {
         if (err)
             return res.send(500, err);
@@ -37,7 +56,7 @@ router.get("/jobSkills", function(req, res) {
     });
 });
 
-router.get("/jobTypes", function(req, res) {
+router.get("/jobTypes", function (req, res) {
     fs.readFile("./sampleData/jobTypes.json", "utf8", function (err, content) {
         if (err)
             return res.send(500, err);
@@ -46,7 +65,7 @@ router.get("/jobTypes", function(req, res) {
     });
 });
 
-router.get("/states", function(req, res) {
+router.get("/states", function (req, res) {
     fs.readFile("./sampleData/states.json", "utf8", function (err, content) {
         if (err)
             return res.send(500, err);
@@ -56,9 +75,9 @@ router.get("/states", function(req, res) {
 });
 
 
-let server = app.listen(8080, function() {
-   let host = server.address().address;
-   let port = server.address().port;
-   
-   console.log("Example app listene", host, port); 
+let server = app.listen(8080, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log("Example app listene", host, port);
 });
