aeeeae106d4d33163fd9cab0f33752cb436c00d5
lesnikovskiy <lesnikovski@gmail.com>
2016-06-16 22:46:35 +0300

    validation plugin

--- CommunityApp/src/jobs/addJob.js
+++ CommunityApp/src/jobs/addJob.js
@@ -1,14 +1,21 @@
 import {inject} from "aurelia-framework";
 import {DataRepository} from "services/dataRepository";
+import {Validation} from "aurelia-validation";
+import {ensure} from 'aurelia-validation';
 
-@inject(DataRepository)
+@inject(DataRepository, Validation)
 export class AddJob {   
     constructor(dataRepository) {
         this.job = {jobType: "Full Time", jobSkills: []};
-        this.dataRepository = dataRepository;
+        this.dataRepository = dataRepository; 
         this.dataRepository.getStates().then(states => this.states = states);
         this.dataRepository.getJobTypes().then(jobTypes => this.jobTypes = jobTypes);
         this.dataRepository.getJobSkills().then(jobSkills => this.jobSkills = jobSkills);
+
+        // this.validation = validation.on(this)
+        //     .ensure("job.title")
+        //     .isNotEmpty()
+        //     .hasMinLength(3);
     }
     
     activate(params, routeConfig, navigationInstruction) {
--- CommunityApp/src/main.js
+++ CommunityApp/src/main.js
@@ -7,7 +7,8 @@ export function configure(aurelia) {
     aurelia.use
         .standardConfiguration()
         .developmentLogging()
-        .plugin('aurelia-dialog');
+        .plugin("aurelia-dialog")
+        .plugin("aurelia-validation");
         
     aurelia.start().then(a => a.setRoot("shell"));
 }
