f9f3fb2ae56a2156783a5c214eaa91dcad4d13fc
lesnikovskiy <lesnikovski@gmail.com>
2016-06-19 20:02:52 +0300

    custom elements

---
+++ CommunityApp/src/common/nav-bar.js
@@ -0,0 +1,29 @@
+import {containerless, customElement, bindable} from "aurelia-framework";
+
+// to render element as regular dom use this attribute
+@containerless()
+// optional, use in case you want to control custom element name
+@customElement("navigation-bar")
+export class NavBar {
+    @bindable router;
+
+    created(view) {
+
+    }
+
+    bind(bindingContext, overrideContext) {
+
+    }
+
+    unbind() {
+
+    }
+
+    attached() {
+
+    }
+
+    detached() {
+
+    }
+}
